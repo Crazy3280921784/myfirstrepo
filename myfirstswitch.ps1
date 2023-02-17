7a43289e5f9e58b07a1e43529d9b21d54837bbb5
Myla <mylapitcher@gmail.com>
2023-02-17 17:56:04 -0500

    adding bubbles condition

--- myfirstswitch.ps1
+++ myfirstswitch.ps1
@@ -4,4 +4,5 @@ switch ($mylist) {
     {$_ -eq "mama"} {Write-Host "$_ didn't actually fart because it's not possible"}
     {$_ -eq "bear"} {Write-Host "$_ skipped farting completely and just pooped his pants."}
     default {Write-Host "$_ farted SOOOOOOOOOOOOOOOOO loudly."}
+    {$_ -eq "bubbles"} {Write-Host "$_ had blasting dirrhea and died before he got to fart really loudly like daddy pig."}
 }
